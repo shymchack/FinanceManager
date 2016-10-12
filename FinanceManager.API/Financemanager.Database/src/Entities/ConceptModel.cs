c977d17d943975406cc5b0f33ef0e90177425901
tomasz.szymczakowski <tomasz.szymczakowski@gmail.com>
2016-10-12 07:37:18 +0200

    Concept db model update. Docs added.

--- FinanceManager.API/Financemanager.Database/src/Entities/ConceptModel.cs
+++ FinanceManager.API/Financemanager.Database/src/Entities/ConceptModel.cs
@@ -5,27 +5,32 @@ namespace FinanceManager.Database.Entities
 {
     public class Account
     {
-        public decimal CurrentAmount { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
         public decimal InitialAmount { get; set; } //TODO: Is it really necessary?
+        public decimal CurrentAmount { get; set; }
 
         public virtual List<User> Users { get; set; }
     }
 
     public class SingleIncome
     {
-        public DateTime IncomeDate { get; set; }
         /// <summary>
         /// Determines whether the income should be processed or not.
         /// </summary>
         public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public IncomeCategory Category { get; set; }
+        /// <summary>
+        /// The amoutn of incoming money. Should be positive
+        /// </summary>
+        public decimal Amount { get; set; }
         /// <summary>
         /// Determines whether the income has been already taken into account.
         /// </summary>
         public bool IsAlreadyProcessed { get; set; }
-        /// <summary>
-        /// The amoutn of incoming money. Should be positive
-        /// </summary>
-        public decimal IncomeAmount { get; set; }
         /// <summary>
         /// Determines whether this is a real or hypothetic income.
         /// </summary>
@@ -36,23 +41,31 @@ namespace FinanceManager.Database.Entities
 
     public class PeriodicIncome
     {
+        public bool IsReal { get; set; }
+        public bool IsActive { get; set; }
         public DateTime ValidityBeginDate { get; set; }
         public DateTime ValidityEndDate { get; set; }
         public short RepetitionUnitQuantity { get; set; } //TODO: Rename
         public PeriodUnit RepetitionUnit { get; set; } //TODO: Rename
         public decimal InitialAmount { get; set; }
+        public string Description { get; set; }
         /// <summary>
         /// Determines whether this is a real or hypothetic income.
         /// </summary>
-        public bool IsReal { get; set; }
-        public bool IsActive { get; set; }
-
-
+        
         public virtual Account Account { get; set; }
+    }
+
+    public class IncomeCategory
+    {
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public IncomeCategory SubCategory { get; set; }
     }
 
     public class User
     {
+        public bool IsActive { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -61,6 +74,7 @@ namespace FinanceManager.Database.Entities
 
     public class UsersAccounts
     {
+        public bool IsActive { get; set; }
         public int UserId { get; set; }
         public int AccountId { get; set; }
 
