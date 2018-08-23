39f26ebc7dde0ecaaf12e876a1a8524e242fea8c
Tomasz Szymczakowski <tomasz.szymczakoski@gmail.com>
2018-08-23 16:46:24 +0200

    Added some react to the index page.

---
+++ FinanceManager.Web/FinanceManager.Web/js/src/components.js
@@ -0,0 +1,11 @@
+﻿class PeriodSummaryHeader extends React.Component {
+    render() {
+        return React.createElement('div', { className: 'col-xs-12 text-center' },
+            React.createElement('h1', null, `${this.props.periodTitle} Summary`));
+    }
+}
+
+ReactDOM.render(
+  React.createElement(PeriodSummaryHeader, { periodTitle: 'October 2018' }, null),
+  document.getElementById('header')
+);
