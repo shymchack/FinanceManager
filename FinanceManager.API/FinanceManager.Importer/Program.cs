7aacb86b2516f8168c1bd0f5b0fb531456f0d791
Tomasz Szymczakowski <tomasz.szymczakoski@gmail.com>
2019-01-22 06:30:09 +0100

    Create importer project and first import data;

---
+++ FinanceManager.API/FinanceManager.Importer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
